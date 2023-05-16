63acc0ebbeb30ca198c1bfbd1dd3f7fa8a122a20
caner24 <cnr24clp@gmail.com>
2023-05-16 13:51:38 +0300

    Added sort algortihm

--- AlgorithmForSpeeds/Form1.cs
+++ AlgorithmForSpeeds/Form1.cs
@@ -18,6 +18,29 @@ namespace AlgorithmForSpeeds
             InitializeComponent();
         }
         private Random rand = new Random(0);
+        int[] array = { 40, 10, 20, 30, 50 };
+        public int[]? NumArray { get; set; }
+
+        public int[] SortArray()
+        {
+            var arrayLength = NumArray.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                var smallestVal = i;
+                for (int j = i + 1; j < arrayLength; j++)
+                {
+                    if (NumArray[j] < NumArray[smallestVal])
+                    {
+                        smallestVal = j;
+                    }
+                }
+                var tempVar = NumArray[smallestVal];
+                NumArray[smallestVal] = NumArray[i];
+                NumArray[i] = tempVar;
+            }
+            return NumArray;
+        }
+
         private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
         {
             // return an array of difting random numbers
