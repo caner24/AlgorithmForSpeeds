eb52e003091977e948dde79d56493b007a50e494
caner24 <cnr24clp@gmail.com>
2023-05-16 13:49:38 +0300

    Add project files.

---
+++ AlgorithmForSpeeds/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AlgorithmForSpeeds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.btnBar = new System.Windows.Forms.Button();
+            this.btnScatter = new System.Windows.Forms.Button();
+            this.btnStem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(256, 81);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(532, 357);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 81);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(137, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 206);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(137, 45);
+            this.trackBar2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox5);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 276);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(137, 162);
+            this.panel1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 71);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(3, 94);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(3, 117);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // btnBar
+            // 
+            this.btnBar.Location = new System.Drawing.Point(316, 52);
+            this.btnBar.Name = "btnBar";
+            this.btnBar.Size = new System.Drawing.Size(75, 23);
+            this.btnBar.TabIndex = 4;
+            this.btnBar.Text = "Bar";
+            this.btnBar.UseVisualStyleBackColor = true;
+            this.btnBar.Click += new System.EventHandler(this.btnBar_Click);
+            // 
+            // btnScatter
+            // 
+            this.btnScatter.Location = new System.Drawing.Point(451, 52);
+            this.btnScatter.Name = "btnScatter";
+            this.btnScatter.Size = new System.Drawing.Size(75, 23);
+            this.btnScatter.TabIndex = 5;
+            this.btnScatter.Text = "Scatter";
+            this.btnScatter.UseVisualStyleBackColor = true;
+            this.btnScatter.Click += new System.EventHandler(this.btnScatter_Click);
+            // 
+            // btnStem
+            // 
+            this.btnStem.Location = new System.Drawing.Point(586, 52);
+            this.btnStem.Name = "btnStem";
+            this.btnStem.Size = new System.Drawing.Size(75, 23);
+            this.btnStem.TabIndex = 6;
+            this.btnStem.Text = "Stem";
+            this.btnStem.UseVisualStyleBackColor = true;
+            this.btnStem.Click += new System.EventHandler(this.btnStem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStem);
+            this.Controls.Add(this.btnScatter);
+            this.Controls.Add(this.btnBar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnBar;
+        private System.Windows.Forms.Button btnScatter;
+        private System.Windows.Forms.Button btnStem;
+    }
+}
+
---
+++ AlgorithmForSpeeds/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AlgorithmForSpeeds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[points];
+            values[0] = start;
+            for (int i = 1; i < points; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
+            return values;
+        }
+        private void btnBar_Click(object sender, EventArgs e)
+        {
+            int pointCount = 5;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindY(ys1);
+            series1.ChartType = SeriesChartType.Column;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindY(ys2);
+            series2.ChartType = SeriesChartType.Column;
+
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Column Chart ({pointCount} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+
+        private void btnScatter_Click(object sender, EventArgs e)
+        {
+            // generate some random XY data
+            int pointCount = 1_000;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindXY(xs1, ys1);
+            series1.ChartType = SeriesChartType.Line;
+            series1.MarkerStyle = MarkerStyle.Circle;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindXY(xs2, ys2);
+            series2.ChartType = SeriesChartType.Line;
+            series2.MarkerStyle = MarkerStyle.Circle;
+
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Scatter Plot ({pointCount:N0} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+
+        private void btnStem_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 100_000;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindY(ys1);
+            series1.ChartType = SeriesChartType.FastLine;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindY(ys2);
+            series2.ChartType = SeriesChartType.FastLine;
+
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Fast Line Plot ({pointCount:N0} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+    }
+}
